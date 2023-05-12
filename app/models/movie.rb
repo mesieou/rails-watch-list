2fcf5ec452d4c2df87f6f8b8ad5181fc28d11c9b
juan bernal <mesieou@gmail.com>
2023-05-12 15:05:18 +0900

    rails watch-list with images upload

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,4 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, uniqueness: true, presence: true
-  has_one_attached :photo
 end
