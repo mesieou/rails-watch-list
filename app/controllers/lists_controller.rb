71661fbed957b51090363ea73ff6cc148818bc7d
juan bernal <mesieou@gmail.com>
2023-05-12 16:03:54 +0900

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list)
     else
-      render new_list_path
+      render new_list_path, status: :unprocessable_entity
     end
   end
 
