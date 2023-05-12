488cb43f6e3d915683ae2885ebab9eb924a45178
juan bernal <mesieou@gmail.com>
2023-05-12 15:24:59 +0900

    rails-watch-list migration added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, except: [ :edit, :update, :destroy ] do
+  resources :lists, except: [ :edit, :update ] do
     resources :bookmarks, only: [ :new, :create]
   end
   delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark
