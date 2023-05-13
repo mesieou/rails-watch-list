8533ebe2e3a9091cf3535276fb59f851e61a4c44
juan bernal <mesieou@gmail.com>
2023-05-13 15:21:17 +0900

    with the api test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def generate_response
+    api_key = ENV.fetch("OPENAI_ACCESS_KEY")
+    engine = "text-davinci-003"
+    prompt = "I have a meeting next weeek about concers related to RFP Evaluation, can you please provide a 3 objectives and an agenda"
+
+    openai_client = OpenAI::Client.new(api_key: api_key, default_engine: engine)
+    response = openai_client.completions(prompt: prompt, max_tokens: 4000, engine: engine)
+    text = response.choices[0].text
+  end
 end
